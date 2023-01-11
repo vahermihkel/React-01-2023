f403f7d480b8a391f1acb87ebe56afeb19b2c109
mihkelvaher <mihkel.vaher@industry62.com>
2023-01-11 17:06:58 +0200

    array'd: sort, filter, map, splice, slice, push

--- veebipood/src/pages/Poed.js
+++ veebipood/src/pages/Poed.js
@@ -1,7 +1,149 @@
+import { useRef, useState } from "react"
 
 function Poed() {
+
+// array --- väärtuste kogumik    
+  // const [kogus, muudaKogus] = useState(0)
+  // const [kogus, muudaKogus] = useState([20,12,44,54,213])
+
+// pean panema array'sse kui tahan sorteerida, filtreerida
+// kui nad tulevad andmebaasist ehk klient tahab ise hallata
+  const [poed, muudaPoed] = useState(["Ülemiste", "Viimsi", "Rocca al Mare", "Magistral", "Vesse", "Kristiine", "Järveotsa"]);
+  const poodRef = useRef();
+
+  const tagasiOriginaali = () => {
+    muudaPoed(["Ülemiste", "Viimsi", "Rocca al Mare", "Magistral", "Vesse", "Kristiine", "Järveotsa"]);
+  }
+
+  const sorteeriAZ = () => {
+    // poed.sort();
+    poed.sort((a, b) => a.localeCompare(b, "et"));
+    // poed.sort((a, b) =>  (a > b) ? 1 : -1);  // .sort() -- default sorting, mis teebki A-Z
+    muudaPoed(poed.slice());
+  }
+
+  const sorteeriZA = () => {
+    // poed.sort().reverse();
+    poed.sort((a, b) => b.localeCompare(a, "et"));  // .sort() -- default sorting, mis teebki A-Z
+    // poed.sort((a, b) =>  (a < b) ? 1 : -1);
+    muudaPoed(poed.slice());
+  }
+
+  const sorteeriTahtKasv = () => {
+    poed.sort((a, b) => a.length - b.length);
+    muudaPoed(poed.slice());
+  }
+
+  const sorteeriTahtKah = () => {
+    poed.sort((a, b) => b.length - a.length);
+    // poed.sort((a, b) => -1 * (a.length - b.length));
+    muudaPoed(poed.slice());
+  }
+
+  const sorteeriTeiseTaheJargi = () => {
+    poed.sort((a, b) => a.charAt(1).localeCompare(b.charAt(1), "et"));
+    muudaPoed(poed.slice());
+  }
+
+  const filtreeriELoppevad = () => {
+    const tulem = poed.filter(e => e.endsWith("e"));
+    muudaPoed(tulem);
+  }
+
+  const filtreeri9Tahega = () => {
+    const tulem = poed.filter(e => e.length === 9);
+    muudaPoed(tulem);
+  }
+
+  const filtreeriKellelIs = () => {
+    const tulem = poed.filter(e => e.includes("is"));
+    muudaPoed(tulem);
+  }
+
+  const filtreeriKellel3sTahtI = () => {
+    const tulem = poed.filter(e => e.charAt(2) === "i");
+    muudaPoed(tulem);
+  }
+
+  const filtreeriKellelTahtiRohkemKui8 = () => {
+    const tulem = poed.filter(e => e.length > 8);
+    muudaPoed(tulem);
+  }
+
+  const muudaK6ikSuureks = () => {
+    const tulem = poed.map(e => e.toUpperCase());
+    muudaPoed(tulem);
+  }
+
+  const muudaK6ikV2ikseks = () => {
+    const tulem = poed.map(e => e.toLowerCase());
+    muudaPoed(tulem);
+  }
+
+  const muudaK6igileT2htedeArvuNumberL6ppu = () => {
+    const tulem = poed.map(e => e + e.length);
+    muudaPoed(tulem);
+  }
+
+  const muudaK6igileKriipsudEtte = () => {
+    const tulem = poed.map(e => "--" + e); // "Vesse" =>  "--Vesse"  =>  "----Vesse" 
+    muudaPoed(tulem);
+  }
+
+  const muudaK6igileIAsemelO = () => {
+    const tulem = poed.map(e => e.replaceAll("i","o"));
+    muudaPoed(tulem);
+  }
+
+  const kustuta = (i) => {
+    poed.splice(i,1); // (mitmendat ma kustutan ,KOMA mitu tk kustutan)
+    muudaPoed(poed.slice()); // koopia tegemine
+  }
+
+  const lisa = () => {
+    poed.push(poodRef.current.value);
+    muudaPoed(poed.slice());
+  }
+
   return (
-    <div>Poed</div>
+    <div>
+      <div>
+        <button onClick={tagasiOriginaali}>Tagasi originaali</button>
+        <button onClick={sorteeriAZ}>Sorteeri A-Z</button>
+        <button onClick={sorteeriZA}>Sorteeri Z-A</button>
+        <button onClick={sorteeriTahtKasv}>Sorteeri tähtede arv kasvavalt</button>
+        <button onClick={sorteeriTahtKah}>Sorteeri tähtede arv kahanevalt</button>
+        <button onClick={sorteeriTeiseTaheJargi}>Sorteeri teise tähe järgi</button>
+        <button onClick={filtreeriELoppevad}>Jäta alles tähega "e" lõppevad</button>
+        <button onClick={filtreeri9Tahega}>Jäta alles 9 tähelised</button>
+        <button onClick={filtreeriKellelIs}>Jäta alles kellel 'is' keskuse nimes</button>
+        <button onClick={filtreeriKellel3sTahtI}>Jäta alles kellel kolmas täht 'i'</button>
+        <button onClick={filtreeriKellelTahtiRohkemKui8}>Jäta alles kellel tähti rohkem kui 8</button>
+        <button onClick={muudaK6ikSuureks}>Muuda kõikidel suurteks tähtedeks</button>
+        <button onClick={muudaK6ikV2ikseks}>Muuda kõikidel väikesteks tähtedeks</button>
+        <button onClick={muudaK6igileT2htedeArvuNumberL6ppu}>Muuda kõikidele number lõppu</button>
+        <button onClick={muudaK6igileKriipsudEtte}>Muuda kõikidele kriipsud ette</button>
+        <button onClick={muudaK6igileIAsemelO}>Muuda kõikidel I asemel O</button>
+      </div>
+
+      <label>Uue poe nimi</label>
+      <input ref={poodRef} type="text" />
+      <button onClick={lisa}>Lisa uus pood</button>
+
+      {poed.map((element,index) => 
+        <div key={index}>{element}. Lahtiolekuaeg:  <button onClick={() => kustuta(index)}>x</button> </div>
+        )}
+      <div>-----------------------------</div>
+      <div>Ülemiste. Lahtiolekuaeg: </div>
+      <div>Viimsi. Lahtiolekuaeg: </div>
+      <div>Rocca al Mare. Lahtiolekuaeg: </div>
+      <div>Magistral. Lahtiolekuaeg: </div>
+      <div>Vesse. Lahtiolekuaeg: </div>
+      <div>Kristiine. Lahtiolekuaeg: </div>
+      <div>Järveotsa. Lahtiolekuaeg: </div>
+      <div>----------</div>
+      {["Saab", "Ford", "Opel", "Audi"].map((auto,j2rjekorraNumber) => <button key={j2rjekorraNumber}>{j2rjekorraNumber}. {auto}</button> )}
+    </div>
   )
 }
 
--- veebipood/src/pages/Seaded.js
+++ veebipood/src/pages/Seaded.js
@@ -12,20 +12,27 @@ function Seaded() {
                           // kui võtit ei ole olemas, on väärtuse taga "null"
   const [keel, uuendaKeel] = useState(localStorage.getItem("keel") || "ee");
 
-  const muudaKeelEE = () => {
-    uuendaKeel("ee");
-    localStorage.setItem("keel","ee"); // salvestus ehk saatmine brauseri lokaalmällu
+  // const muudaKeelEE = () => {
+  //   uuendaKeel("ee");
+  //   localStorage.setItem("keel","ee"); // salvestus ehk saatmine brauseri lokaalmällu
+  // }
+
+  // const muudaKeelEN = () => {
+  //   uuendaKeel("en");
+  //   localStorage.setItem("keel","en");
+  // }
+
+  // const muudaKeelRU = () => {
+  //   uuendaKeel("ru");
+  //   localStorage.setItem("keel","ru");
+  // }
+
+  const muudaKeel = (uusKeel) => {
+    uuendaKeel(uusKeel);
+    localStorage.setItem("keel",uusKeel)
   }
 
-  const muudaKeelEN = () => {
-    uuendaKeel("en");
-    localStorage.setItem("keel","en");
-  }
-
-  const muudaKeelRU = () => {
-    uuendaKeel("ru");
-    localStorage.setItem("keel","ru");
-  }
+  
 
   const emailLuger = useRef();
 
@@ -49,15 +56,17 @@ function Seaded() {
       <input ref={telefonLuger} type="text" />
       <button onClick={salvestaTelefon}>Sisesta</button>
       <br />
-      <button onClick={muudaKeelEE}>Eesti</button>
-      <button onClick={muudaKeelEN}>English</button>
-      <button onClick={muudaKeelRU}>Pycckuj</button>
+      <button onClick={() => muudaKeel("ee")}>Eesti</button>
+      <button onClick={() => muudaKeel("en")}>English</button>
+      <button onClick={() => muudaKeel("ru")}>Pycckuj</button>
       {keel === "ee" && <div>Leht on eesti keeles</div>}
       {keel === "en" && <div>Page is in English</div>}
       {keel === "ru" && <div>leht_on_eesti_keeles RU</div>}
     </div>
   )
 }
+// onClick={muuda}    onClick={() => muuda("te")}   
+
 
 // ee / est    otsida projektist kas on juba mingeid keeleasju tehtud ja jätkata sama teed
 // valida, aga siis kasutada kõikide keelte jaoks sama lühendit ee  +  ru   + en     est + eng + rus
